d97783245d769aec812e77a18888e297db5adad0
LinuxDaFaHao <Wanghaoxin2014@outlook.com>
2019-05-17 00:44:44 +0800

    Accelerate

--- Wang-Landau.py
+++ Wang-Landau.py
@@ -28,6 +28,13 @@ flat_para = 0.8
 sample_time = 2000000
 min_factor = 1+pow(10,-8)
 
+# Bond connection, Conn[i] is the sites connected to i-th site
+Conn=[[1,4,6],[0,2,9],[1,3,12],[2,4,15],[3,0,18],
+      [6,19,20],[0,5,7],[6,8,21],[7,9,24],[1,8,10],[9,25,11],[10,12,28],[2,11,13],[12,14,29],[13,15,32],[3,14,16],[15,17,33],[16,18,36],[17,4,19],[18,5,37],
+      [5,21,39],[7,20,22],[21,23,40],[22,24,42],[8,23,25],[10,24,26],[43,25,27],[26,28,45],[27,29,11],[28,30,13],[29,31,46],[30,32,48],[31,33,14],[32,34,16],[33,35,49],[34,36,51],[35,37,17],[36,38,19],[37,39,52],[38,20,54],
+      [41, 54, 22], [42, 55, 40], [43, 41, 23], [44, 42, 26], [56, 45, 43], [46, 44, 27], [47, 45, 30], [57, 48, 46], [49, 47, 31], [50, 34, 48], [58, 51, 49], [52, 50, 35], [53, 51, 38], [59, 54, 52], [53, 40, 39],
+      [56, 59, 41], [57, 55, 44], [58, 56, 47], [59, 57, 50], [58, 55, 53]]
+
 def getEnergy(S):
     Energy = 0
     
@@ -51,6 +58,11 @@ def getEnergy(S):
         
     return Energy
 
+def getLocalEn(S,k):
+# calculate the energy on the bonds which are connected to k-th site
+    LocalEn =J*S[k]*S[Conn[k][0]] + J*S[k]*S[Conn[k][1]] + J*S[k]*S[Conn[k][2]]
+
+    return LocalEn
 
 if __name__ == "__main__":
     print("==============================================")
@@ -69,28 +81,19 @@ if __name__ == "__main__":
         H = np.zeros(len(En))
         print('step',j,'f=',  f, end='')
         S = [random.randint(0,1)*2-1 for i in range(L)] # initial a random state
-        S_flip=[0 for l in range(L)]
-        for l in range(L):
-               S_flip[l]=S[l]
+        E = getEnergy(S)
+        indexE= En.index(E)
         for i in range(sample_time):
             tag=0
-            E1=getEnergy(S)
-            
             k = random.randint(0,L-1)
-            S_flip[k]=-S_flip[k]
+            E2=E-2*getLocalEn(S,k)
+            
+            index2= En.index(E2)
+            if(g[indexE] >= g[index2] or (g[indexE] < g[index2] and random.random()<=g[indexE]/g[index2])):
+                S[k]=-S[k]
+                E=E2
+                indexE=index2
 
-            E2=getEnergy(S_flip)
-
-            index1=int((E1+90)/2)
-            index2=int((E2+90)/2)
-            if(g[index1] >= g[index2] or (g[index1] < g[index2] and random.random()<=g[index1]/g[index2])):
-               S[k]=-S[k]
-               E=E2
-            else:
-               E=E1
-               S_flip[k]=-S_flip[k]
-
-            indexE=int((E+90)/2)
             if g[indexE]>pow(10,20):
                 g=g/sum(g)*pow(10,18)
                 
